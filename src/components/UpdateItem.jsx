6e4e4795317a22db7a4c696e3e7aaabf110467b0
arbin-mahato <proarbind@gmail.com>
2025-03-31 10:48:06 +0530

    task done

--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const UpdateItem = ({ itemId, API_URI }) => {
   const [item, setItem] = useState(null);
